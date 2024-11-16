9bc7f57bbabe83780d1633080a08c8cd38ea2770
Phani Murari <imphanimurari1@gmail.com>
2024-11-16 11:02:13 +0530

    Changed the Text in Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,18 @@
+import Button from "@/components/common/Button";
+import starsBg from '@/assets/stars.png'
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return <section className="h-[492px] flex items-center"
+  style={{backgroundImage: `url(${starsBg.src})`}}
+  >
+    <div className="container">
+      <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">Ai DO it For Us</h1>
+      <p className="text-lg text-white/70 mt-5 text-center">Helping Devs to boost the productivity</p>
+      <div className="flex justify-center mt-5">
+      <Button>
+        Join Waitlist
+      </Button>
+      </div>
+    </div>
+  </section>;
 };
