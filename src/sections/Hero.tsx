f259c8ebca9951dbcbeb1185349ab78a60af5b4d
aidoitdev <aidoit.dev@gmail.com>
2024-11-24 22:24:04 +0530

    Changed-slogan

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,8 +7,10 @@ export const Hero = () => {
   style={{backgroundImage: `url(${starsBg.src})`}}
   >
     <div className="container">
-      <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">Ai DO iT</h1>
-      <p className="text-lg text-white/70 mt-5 text-center">Helping Devs to boost the productivity</p>
+      <h1 className="text-6xl md:text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">Ai DO iT</h1>
+      <p className="text-lg text-white/70 mt-5 text-center">
+        Use <span className="font-bold text-theme-color">Ai DO iT</span>—code smart, not like an <span className="font-bold line-through">iDoiT</span>!
+      </p>
       <EmailInput/>
     </div>
   </section>;
